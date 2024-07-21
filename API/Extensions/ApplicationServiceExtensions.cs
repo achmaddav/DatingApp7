a46522d68aaf0006d08bff16a96eac1b204a63f3
achmaddav <achmaddavit92@gmail.com>
2024-07-21 15:21:59 +0700

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,8 @@ namespace API.Extensions
             });
             services.AddCors(); // setting api to angular
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
